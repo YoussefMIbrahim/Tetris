d0686b4075217dd4506819f68ae8c6b3c5b48ede
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-03-16 13:56:56 -0500

    slowly making this work

--- tetris_script.js
+++ tetris_script.js
@@ -24,32 +24,32 @@ function buttonInputs() {
         // console.log(event['keyCode']);
         if (event['keyCode'] === 37){
             if (checkSideCollisions("LEFT")) {
-                block.xPos -= 50;
                 clear();
                 drawGrid();
                 checkBlockCollisions();
                 block.bottom();
+                block.xPos -= 50;
                 drawArrayBlocks();
                 block.draw();
             }
         }
         else if (event['keyCode'] === 39){
             if (checkSideCollisions("RIGHT")) {
-                block.xPos += 50;
                 clear();
                 drawGrid();
                 checkBlockCollisions();
                 block.bottom();
+                block.xPos += 50;
                 drawArrayBlocks();
                 block.draw();
             }
         }
         else if (event['keyCode'] === 40){
-            block.yPos += 50;
             clear();
             drawGrid();
             checkBlockCollisions();
             block.bottom();
+            block.yPos += 50;
             drawArrayBlocks();
             block.draw();
         }
@@ -100,8 +100,8 @@ function updateGame (){
     clear();
     drawGrid();
     block.bottom();
-    checkBlockCollisions();
     drawArrayBlocks();
+    checkBlockCollisions();
     block.draw();
     block.update();
 
@@ -153,10 +153,10 @@ function checkBlockCollisions() {
 
             for (let x = 0; x < block.blocksCoordinates.length; x++) {
 
-                if (((block.blocksCoordinates[0]['y'] === (cBlock.blocksCoordinates[x]['y'] - 100)) ||
-                    (block.blocksCoordinates[1]['y'] === (cBlock.blocksCoordinates[x]['y'] - 100)) ||
-                    (block.blocksCoordinates[2]['y'] === (cBlock.blocksCoordinates[x]['y'] - 100)) ||
-                    (block.blocksCoordinates[3]['y'] === (cBlock.blocksCoordinates[x]['y'] - 100))) &&
+                if (((block.blocksCoordinates[0]['y'] === (cBlock.blocksCoordinates[x]['y'] - blockSize)) ||
+                    (block.blocksCoordinates[1]['y'] === (cBlock.blocksCoordinates[x]['y'] - blockSize)) ||
+                    (block.blocksCoordinates[2]['y'] === (cBlock.blocksCoordinates[x]['y'] - blockSize)) ||
+                    (block.blocksCoordinates[3]['y'] === (cBlock.blocksCoordinates[x]['y'] - blockSize))) &&
                     ((block.blocksCoordinates[0]['x'] === (cBlock.blocksCoordinates[x]['x'])) ||
                     (block.blocksCoordinates[1]['x'] === (cBlock.blocksCoordinates[x]['x'] )) ||
                     (block.blocksCoordinates[2]['x'] === (cBlock.blocksCoordinates[x]['x'] )) ||
